d9e53095f678fb824e546480598ca0634a84f6c0
Martin Scheiber <martin.scheiber@aau.at>
2022-01-05 08:35:04 +0100

    [MAINT] Minor debug changes

--- include/uav_init/uwb_init.cpp
+++ include/uav_init/uwb_init.cpp
@@ -368,23 +368,16 @@ bool UwbInitializer::initialize_double_all(UwbAnchorBuffer& anchor_buffer, const
               ((svd.matrixV().transpose()).inverse() * svd.singularValues().asDiagonal().inverse() *
                svd.singularValues().asDiagonal().inverse() * svd.matrixV().inverse());
 
+          /// \todo TODO(scm): make this debug again
           INIT_DEBUG_STREAM("\n\tCov:        " << Cov);
+          //          INIT_INFO_STREAM("\n\tCov:        " << Cov);
 
-          // compare norm of singular values vector of covarnace matrix with threshold
-          Eigen::JacobiSVD<Eigen::MatrixXd> svd_cov(Cov, Eigen::ComputeThinU | Eigen::ComputeThinV);
-          if (svd_cov.singularValues().norm() <= params_.cov_sv_threshold_)
-          {
-            new_uwb_anchor.initialized = true;
-            successfully_initialized = true;
-          }
-          else
-          {
-            INIT_WARN_STREAM("Anchor " << anchor_id << ": issue with cov svd threshold (" << svd_cov.singularValues().norm()
-                                       << ")");
-            new_uwb_anchor.initialized = false;
-            successfully_initialized = false;
-          }
+          // debug
+          INIT_INFO_STREAM("\tsingular values:   " << svd_cov.singularValues().transpose());
+          INIT_INFO_STREAM("\tsingular v norm:   " << svd_cov.singularValues().norm());
 
+          // set initialization to true
+          new_uwb_anchor.initialized = true;
         }
         else  // if (LSSolution[0] > 0)
         {
